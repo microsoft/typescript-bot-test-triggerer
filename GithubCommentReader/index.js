41f5d5cb3cadd418d0444530763956d94a246c56
Wesley Wigham <wewigham@microsoft.com>
2018-08-02 17:16:59 -0700

    And it works, at least for branches on the main repo

--- GithubCommentReader/index.js
+++ GithubCommentReader/index.js
@@ -5,54 +5,59 @@ const crypto = require("crypto");
 
 module.exports = async function (context, data) {
     const sig = data.headers["x-hub-signature"];
-    const hmac = crypto.createHmac("sha1", "jaH7T1WULO9l7hfAI16BG0MUSK5ruWnB4kiGIxaOktTjCa9YFWkCow==");
+    const hmac = crypto.createHmac("sha1", process.env.WEBHOOK_TOKEN);
     hmac.write(data.rawBody);
     const digest = hmac.digest();
     if (!sig || !crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(`sha1=${digest.toString("hex")}`))) {
+        // context.log(`Discarding request with mismatched signature: ${sig}`)
         return context.done();
     }
-    const shouldHandleComment = data.action === "created" && data.comment && data.comment.body && data.pull_request && matchesCommand(data.comment.body);
+    const request = data.body;
+    const shouldHandleComment = request.action === "created" && request.comment && request.comment.body && (request.pull_request || request.issue && request.issue.pull_request) && matchesCommand(request.comment.body);
     if (!shouldHandleComment) {
+        // context.log(`Discarding filtered request: action: ${request.action}, isComment: ${!!request.comment} body: ${request.comment && request.comment.body}, isPr: ${!!(request.pull_request || request.issue && request.issue.pull_request)}, matches: ${matchesCommand(request.comment && request.comment.body)}`);
         return context.done();
     }
-    const requestingUserStatus = data.comment.user.author_association;
+    const requestingUserStatus = request.comment.author_association;
     if (requestingUserStatus !== "MEMBER" && requestingUserStatus !== "OWNER" && requestingUserStatus !== "COLLABORATOR") {
+        // context.log(`Discarding via association: ${request.comment.user.login}: ${requestingUserStatus}`);
         return context.done(); // Only trigger for MS members/repo owners/invited collaborators
     }
-    context.log('GitHub Webhook triggered!', data.comment.body);
+    context.log('GitHub Webhook triggered!', request.comment.body);
     const cli = new Client();
     cli.authenticate({
         type: "token",
-        token: context.bindings.github_token
+        token: process.env.GITHUB_TOKEN
     });
-    const refSha = data.pull_request.head.sha;
-    const branch = data.pull_request.head.ref;
-    const originUrl = data.pull_request.head.repo.git_url;
-    const isLocalBranch = originUrl === "git@github.com:Microsoft/TypeScript.git";
-    const requestingUser = data.comment.user.login;
-    const result = await cli.pullRequests.createCommentReply({
-        body: `Heya @${requestingUser}, I'm starting to run the extended test suite on this PR at ${refSha}. Hold tight - I'll update this comment with the results when it's done.`,
-        in_reply_to: data.comment.id,
-        number: data.pull_request.number,
+    const pr = request.pull_request || (await cli.pullRequests.get({ number: request.issue.number, owner: "Microsoft", repo: "TypeScript" })).data;
+    const refSha = pr.head.sha;
+    const branch = pr.head.ref;
+    const originUrl = pr.head.repo.git_url;
+    context.log(originUrl);
+    const requestingUser = request.comment.user.login;
+    const result = await cli.issues.createComment({
+        body: `Heya @${requestingUser}, I'm starting to run the extended test suite on this PR at ${refSha}. Hold tight - I'll update this comment with the log link once the build has been queued.`,
+        number: pr.number,
         owner: "Microsoft",
         repo: "TypeScript"
     });
-    const commentId = result.data.comment.id;
-    const vcli = new vsts.WebApi("https://typescript.visualstudio.com/defaultcollection", vsts.getPersonalAccessTokenHandler(context.bindings.vsts_token)); 
+    const commentId = result.data.id;
+    const vcli = new vsts.WebApi("https://typescript.visualstudio.com/defaultcollection", vsts.getPersonalAccessTokenHandler(process.env.VSTS_TOKEN)); 
     const build = await vcli.getBuildApi();
+    const isLocalBranch = originUrl === "git://github.com/Microsoft/TypeScript.git";
     const buildQueue = await build.queueBuild(/** @type {*} */({
         definition: { id: 11 },
         queue: { id: 8 },
         project: { id: "cf7ac146-d525-443c-b23c-0d58337efebc" },
         sourceBranch: isLocalBranch ? branch : "master", // Undocumented, but used by the official frontend
         sourceVersion: isLocalBranch ? refSha : "", // Also undocumented
-        parameters: JSON.stringify(isLocalBranch ? { status_comment: commentId } : { pr_id: data.pull_request.number, remote_url: originUrl, remote_branch: branch, remote_sha: refSha, status_comment: commentId }) // This API is garbage
+        parameters: JSON.stringify(isLocalBranch ? { status_comment: commentId } : { pr_id: pr.number, remote_url: originUrl, remote_branch: branch, remote_sha: refSha, status_comment: commentId }) // This API is garbage
     }), "TypeScript");
-    await cli.pullRequests.editComment({
+    await cli.issues.editComment({
         owner: "Microsoft",
         repo: "TypeScript",
         comment_id: commentId,
-        body: `Heya @${requestingUser}, I've started to run the extended test suite on this PR at ${refSha}. You can monitor the build [here](${buildQueue._links.web.href}). Hold tight - I'll update this comment with the results when it's done.`
+        body: `Heya @${requestingUser}, I've started to run the extended test suite on this PR at ${refSha}. You can monitor the build [here](${buildQueue._links.web.href}). It should now contribute to this PR's status checks.`
     });
     context.done();
 };
